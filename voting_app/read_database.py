87a1dca18a8061f79d103adff01bca732bd4f227
Jefferson Tan <jefferson1990@yahoo.com>
2021-11-01 15:31:03 +0800

    lint

--- voting_app/read_database.py
+++ voting_app/read_database.py
@@ -1,27 +1,35 @@
-import sqlalchemy as sqla
 import csv
-from environs import Env
+
 import pandas as pd
+import sqlalchemy as sqla
 from sqlalchemy import func
 
+from environs import Env
 from voting_app import commands, vote
+
 
 def get_data():
     Voter = vote.models.Voter
     Vote = vote.models.Vote
 
-    df = pd.DataFrame(Vote.query.with_entities(Vote.type, Vote.name, func.count(Vote.id))\
-            .group_by(Vote.type, Vote.name).all(), columns=['type', 'name', 'count'])
+    df = pd.DataFrame(
+        Vote.query.with_entities(Vote.type, Vote.name, func.count(Vote.id))
+        .group_by(Vote.type, Vote.name)
+        .all(),
+        columns=["type", "name", "count"],
+    )
 
     return df
+
 
 def get_otp(fname, lname):
     Voter = vote.models.Voter
 
-    results = Voter.query.filter(Voter.first_name.like(fname),
-                             Voter.last_name.like(lname))\
-                         .with_entities(Voter.first_name, Voter.last_name, Voter.otp, Voter.voted)\
-                         .all()
+    results = (
+        Voter.query.filter(Voter.first_name.like(fname), Voter.last_name.like(lname))
+        .with_entities(Voter.first_name, Voter.last_name, Voter.otp, Voter.voted)
+        .all()
+    )
 
     for r in results:
         if r[3]:
--- voting_app/start_database.py
+++ voting_app/start_database.py
@@ -1,9 +1,20 @@
-import sqlalchemy as sqla
 import csv
-from environs import Env
-import pandas as pd
 
-def create_test_user(db_url=None, first_name="FTest", last_name="LTest", otp="PASS1234", email=None, mobile=None, voted=False):
+import pandas as pd
+import sqlalchemy as sqla
+
+from environs import Env
+
+
+def create_test_user(
+    db_url=None,
+    first_name="FTest",
+    last_name="LTest",
+    otp="PASS1234",
+    email=None,
+    mobile=None,
+    voted=False,
+):
     if not db_url:
         env = Env()
         env.read_env()
@@ -14,22 +25,27 @@ def create_test_user(db_url=None, first_name="FTest", last_name="LTest", otp="PA
     metadata = sqla.MetaData(bind=engine)
     metadata.reflect()
 
+    voters = metadata.tables["voters"]
 
-    voters = metadata.tables['voters']
-
-    test_voter = voters.insert().values(first_name=first_name, last_name=last_name,
-            otp=otp, email=email,
-            mobile=mobile, voted=voted)
+    test_voter = voters.insert().values(
+        first_name=first_name,
+        last_name=last_name,
+        otp=otp,
+        email=email,
+        mobile=mobile,
+        voted=voted,
+    )
 
     with engine.connect() as conn:
         result = conn.execute(test_voter)
     print("Done.")
 
+
 def load_users(file_path="./db_users.csv", db_url=None):
     print("Started...")
 
     df_csv = pd.read_csv(file_path)
-    df_csv['voted'] = False
+    df_csv["voted"] = False
 
     if not db_url:
         env = Env()
@@ -38,5 +54,5 @@ def load_users(file_path="./db_users.csv", db_url=None):
     engine = sqla.create_engine(db_url, echo=True)
 
     with engine.connect() as conn:
-        df_csv.to_sql('voters', conn, if_exists='replace', index=True, index_label="id")
+        df_csv.to_sql("voters", conn, if_exists="replace", index=True, index_label="id")
     print("Done.")
